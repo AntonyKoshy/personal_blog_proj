495e75f7f9381e7cd52e14921668d7c5637c45be
AntonyKoshy <antony.tk.10@gmail.com>
2025-10-20 21:00:07 +0530

    feat: added JWT auth for admin endpoints, created tables struct

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"time"
 	"context"
 	"github.com/jackc/pgx/v5"
 	"log"
 	"fmt"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 // Define constants for your DB connection parameters
@@ -12,8 +14,17 @@ const (
     dbUser     = "navyapillai"
     dbHost     = "localhost"
     dbPort     = 5432
-    dbName     = "postgres"
+    dbName     = "blogdb"
 )
+
+// Struct representing articles table
+type Article struct {
+    ID        int       `json:"id"`
+    Content   string    `json:"content"`
+    CreatedAt time.Time `json:"created_at"`
+    UpdatedAt time.Time `json:"updated_at"`
+}
+
 
 func main() {
 
@@ -41,3 +52,45 @@ func main() {
 
 
 }
+
+func generateJWT(username string) (string, error){
+	claims := &jwt.RegisteredClaims{
+		Issuer:    "kundan blogs",
+		Subject:   username,
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)), // Token expires in 24 hours
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func jwtMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        tokenStr := r.Header.Get("Authorization")
+        if tokenStr == "" {
+            http.Error(w, "Missing token", http.StatusUnauthorized)
+            return
+        }
+
+        // Usually token comes as "Bearer <token>", so parse it
+        parts := strings.Split(tokenStr, " ")
+        if len(parts) != 2 || parts[0] != "Bearer" {
+            http.Error(w, "Invalid token format", http.StatusUnauthorized)
+            return
+        }
+        tokenStr = parts[1]
+
+        token, err := jwt.ParseWithClaims(tokenStr, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+            return jwtKey, nil
+        })
+
+        if err != nil || !token.Valid {
+            http.Error(w, "Invalid token", http.StatusUnauthorized)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
+}
+
+
